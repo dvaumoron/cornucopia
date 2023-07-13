93591c780d40448c6182f83b30d016fa7084cd26
dvaumoron <dvaumoron@gmail.com>
2023-07-13 09:37:00 +0200

    improving test

--- examples/model/model.go
+++ examples/model/model.go
@@ -10,18 +10,24 @@ type RowQueryerContext interface {
 	QueryRowContext(ctx context.Context, query string, args ...any) *sql.Row
 }
 
-func MakeUser(name string, email string) User {
+type User struct {
+	Name  string
+	Email string
+}
+
+func MakeUser(Name string, Email string) User {
 	return User{
-		email: email,
-		name:  name,
+		Email: Email,
+		Name:  Name,
 	}
 }
-func QueryUser(pool RowQueryerContext, ctx context.Context, id int64) (User, error) {
-	ctx, cancel := context.WithTimeout(ctx, int64(5)*time.Second)
+
+func QueryUser(pool RowQueryerContext, ctx context.Context, name string) (User, error) {
+	ctx, cancel := context.WithTimeout(ctx, time.Duration(int64(5))*time.Second)
 	defer cancel()
 
-	var name string
-	var email string
-	err := pool.QueryRowContext(ctx, "select u.name, u.email from user as u where u.id = :id;", sql.Named("id", id)).Scan(&name, &email)
-	return MakeUser(name, email), err
+	var Name string
+	var Email string
+	err := pool.QueryRowContext(ctx, "select u.name, u.email from user as u where u.name = :name;", sql.Named("name", name)).Scan(&Name, &Email)
+	return MakeUser(Name, Email), err
 }
