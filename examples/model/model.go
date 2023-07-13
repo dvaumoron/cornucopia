6e36d929205bb72a5eccf7791af367f493cd18ad
dvaumoron <dvaumoron@gmail.com>
2023-07-13 14:09:35 +0200

    improve test

--- examples/model/model.go
+++ examples/model/model.go
@@ -30,23 +30,60 @@ func MakeUser(Name string, Email string) User {
 	}
 }
 
-func SaveUser(pool ExecerContext, ctx context.Context, Name string, Email string) (int64, error) {
+func (o User) Create(pool ExecerContext, ctx context.Context) error {
+	_, err := createUser(pool, ctx, o.Name, o.Email)
+	return err
+}
+
+func ReadUser(pool RowQueryerContext, ctx context.Context, name string) (User, error) {
 	ctx, cancel := context.WithTimeout(ctx, timeout)
 	defer cancel()
 
-	result, err := pool.ExecContext(ctx, "insert into users(name, email) values(:Name, :Email)", sql.Named("Name", Name), sql.Named("Email", Email))
+	var Name string
+	var Email string
+	err := pool.QueryRowContext(ctx, "select o.name, o.email from users as o where o.name = :name;", sql.Named("name", name)).Scan(&Name, &Email)
+	return MakeUser(Name, Email), err
+}
+
+func (o User) Update(pool ExecerContext, ctx context.Context) error {
+	_, err := updateUser(pool, ctx, o.Name, o.Email)
+	return err
+}
+
+func (o User) Delete(pool ExecerContext, ctx context.Context) error {
+	_, err := deleteUser(pool, ctx, o.Name)
+	return err
+}
+
+func createUser(pool ExecerContext, ctx context.Context, Name string, Email string) (int64, error) {
+	ctx, cancel := context.WithTimeout(ctx, timeout)
+	defer cancel()
+
+	result, err := pool.ExecContext(ctx, "insert into users(name, email) values(:Name, :Email);", sql.Named("Name", Name), sql.Named("Email", Email))
 	if err != nil {
 		return int64(0), err
 	}
 	return result.RowsAffected()
 }
 
-func GetUserByName(pool RowQueryerContext, ctx context.Context, name string) (User, error) {
+func updateUser(pool ExecerContext, ctx context.Context, Name string, Email string) (int64, error) {
 	ctx, cancel := context.WithTimeout(ctx, timeout)
 	defer cancel()
 
-	var Name string
-	var Email string
-	err := pool.QueryRowContext(ctx, "select u.name, u.email from users as u where u.name = :name;", sql.Named("name", name)).Scan(&Name, &Email)
-	return MakeUser(Name, Email), err
+	result, err := pool.ExecContext(ctx, "update users set email = :Email where name = :Name;", sql.Named("Name", Name), sql.Named("Email", Email))
+	if err != nil {
+		return int64(0), err
+	}
+	return result.RowsAffected()
+}
+
+func deleteUser(pool ExecerContext, ctx context.Context, name string) (int64, error) {
+	ctx, cancel := context.WithTimeout(ctx, timeout)
+	defer cancel()
+
+	result, err := pool.ExecContext(ctx, "delete from users where name = :name;", sql.Named("name", name))
+	if err != nil {
+		return int64(0), err
+	}
+	return result.RowsAffected()
 }
