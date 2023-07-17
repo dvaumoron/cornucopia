1bbb8abe8d92a94a097a98c2b45bf6bf55581fc0
dvaumoron <dvaumoron@gmail.com>
2023-07-17 10:22:42 +0200

    improve test

--- examples/model/model.go
+++ examples/model/model.go
@@ -19,69 +19,75 @@ type RowQueryerContext interface {
 }
 
 type User struct {
-	Name  string
-	Email string
+	Login     string
+	Firstname string
+	Lastname  string
+	Email     string
 }
 
-func MakeUser(name string, email string) User {
+func MakeUser(login string, firstname string, lastname string, email string) User {
 	return User{
-		Email: email,
-		Name:  name,
+		Email:     email,
+		Firstname: firstname,
+		Lastname:  lastname,
+		Login:     login,
 	}
 }
 
 func (o User) Create(pool ExecerContext, ctx context.Context) error {
-	_, err := createUser(pool, ctx, o.Name, o.Email)
+	_, err := createUser(pool, ctx, o.Login, o.Firstname, o.Lastname, o.Email)
 	return err
 }
 
-func ReadUser(pool RowQueryerContext, ctx context.Context, name string) (User, error) {
+func ReadUser(pool RowQueryerContext, ctx context.Context, login string) (User, error) {
 	ctx, cancel := context.WithTimeout(ctx, timeout)
 	defer cancel()
 
-	var Name string
+	var Login string
+	var Firstname string
+	var Lastname string
 	var Email string
-	err := pool.QueryRowContext(ctx, "select o.name, o.email from users as o where o.name = :name;", sql.Named("name", name)).Scan(&Name, &Email)
-	return MakeUser(Name, Email), err
+	err := pool.QueryRowContext(ctx, "select o.login, o.firstname, o.lastname, o.email from users as o where o.login = :login;", sql.Named("login", login)).Scan(&Login, &Firstname, &Lastname, &Email)
+	return MakeUser(Login, Firstname, Lastname, Email), err
 }
 
 func (o User) Update(pool ExecerContext, ctx context.Context) error {
-	_, err := updateUser(pool, ctx, o.Name, o.Email)
+	_, err := updateUser(pool, ctx, o.Login, o.Firstname, o.Lastname, o.Email)
 	return err
 }
 
 func (o User) Delete(pool ExecerContext, ctx context.Context) error {
-	_, err := deleteUser(pool, ctx, o.Name)
+	_, err := deleteUser(pool, ctx, o.Login)
 	return err
 }
 
-func createUser(pool ExecerContext, ctx context.Context, Name string, Email string) (int64, error) {
+func createUser(pool ExecerContext, ctx context.Context, Login string, Firstname string, Lastname string, Email string) (int64, error) {
 	ctx, cancel := context.WithTimeout(ctx, timeout)
 	defer cancel()
 
-	result, err := pool.ExecContext(ctx, "insert into users(name, email) values(:Name, :Email);", sql.Named("Name", Name), sql.Named("Email", Email))
+	result, err := pool.ExecContext(ctx, "insert into users(login, firstname, lastname, email) values(:Login, :Firstname, :Lastname, :Email);", sql.Named("Login", Login), sql.Named("Firstname", Firstname), sql.Named("Lastname", Lastname), sql.Named("Email", Email))
 	if err != nil {
 		return int64(0), err
 	}
 	return result.RowsAffected()
 }
 
-func updateUser(pool ExecerContext, ctx context.Context, Name string, Email string) (int64, error) {
+func updateUser(pool ExecerContext, ctx context.Context, Login string, Firstname string, Lastname string, Email string) (int64, error) {
 	ctx, cancel := context.WithTimeout(ctx, timeout)
 	defer cancel()
 
-	result, err := pool.ExecContext(ctx, "update users set email = :Email where name = :Name;", sql.Named("Name", Name), sql.Named("Email", Email))
+	result, err := pool.ExecContext(ctx, "update users set firstname = :Firstname, lastname = :Lastname, email = :Email where login = :Login;", sql.Named("Login", Login), sql.Named("Firstname", Firstname), sql.Named("Lastname", Lastname), sql.Named("Email", Email))
 	if err != nil {
 		return int64(0), err
 	}
 	return result.RowsAffected()
 }
 
-func deleteUser(pool ExecerContext, ctx context.Context, name string) (int64, error) {
+func deleteUser(pool ExecerContext, ctx context.Context, login string) (int64, error) {
 	ctx, cancel := context.WithTimeout(ctx, timeout)
 	defer cancel()
 
-	result, err := pool.ExecContext(ctx, "delete from users where name = :name;", sql.Named("name", name))
+	result, err := pool.ExecContext(ctx, "delete from users where login = :login;", sql.Named("login", login))
 	if err != nil {
 		return int64(0), err
 	}
